e6fc35e1e19059bdfec13ae426a13a30d6788e1c
cmitche09 <cmitche09@gmail.com>
2020-11-23 11:16:28 -0500

    Strategy

---
+++ src/main/java/strategy/StrategyDemo.java
@@ -0,0 +1,13 @@
+package strategy;
+
+public class StrategyDemo {
+    public static void main(String[] args) {
+        CreditCard amexCard = new CreditCard(new AmexStrategy());
+
+        amexCard.setNumber("379185883464283");
+        amexCard.setDate("04/2020");
+        amexCard.setCvv("123");
+
+        System.out.println("Is Amex valid: " + amexCard.isValid());
+    }
+}
